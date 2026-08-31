2a6414a871d3e1ed4c517c13246946e79868cdb9
raunakKapoor07 <raunak04kapoor@gmail.com>
2026-09-01 00:33:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (60.66%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
-    int minBitFlips(long long start, long long goal) {
-        vector<int> binst(32,0);
-        vector<int>bingl(32,0);
-        long long temp1=start;
-        long long temp2=goal;
-        int i=1;
-        while(temp1>0){
-            binst[32-i]=temp1%2;
-            temp1/=2;
-            i++;
-        }
-        i=1;
-        while(temp2>0){
-            bingl[32-i]=temp2%2;
-            temp2/=2;
-            i++;
-        }
+    int minBitFlips(int start, int goal) {
+        long long xrr=start^goal;
         int ans=0;
-        for(i=0;i<32;i++){
-            if(binst[i]!=bingl[i]){
+        while(xrr>0){
+            if(xrr%2!=0){
                 ans++;
             }
+            xrr=xrr>>1;
         }
         return ans;
     }
