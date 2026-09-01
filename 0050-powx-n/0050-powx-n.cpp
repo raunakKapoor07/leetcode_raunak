3b3be36056c9a44f3a8af99ab5b1b7828ab24e4f
raunakKapoor07 <raunak04kapoor@gmail.com>
2026-09-01 11:13:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.72%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if(n==0){
+            return 1.0;
+        }
+        if(n<0){
+            return 1.0/(myPow(x,-n));
+        }
+        if(n%2==1){
+        return x*myPow(x,n-1);  
+        }
+        else{
+            return myPow(x*x,n/2);
+        }
+    }
+};
