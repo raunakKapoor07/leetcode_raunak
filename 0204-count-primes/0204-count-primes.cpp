c86b434078be972569935bca3f359e9d53b64c06
raunakKapoor07 <raunak04kapoor@gmail.com>
2026-09-02 15:53:48 +0530

    Time: 763 ms (7.7%), Space: 13.5 MB (33.04%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    long long sieveOfEratosthenes(long long n) {
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (long long i = p * p; i <= n; i += p) {
+                prime[i] = false;
+            }
+        }
+    }
+    long long count=0;
+    for (long long p = 2; p <= n; p++) {
+        if (prime[p]) {
+            count++; 
+        }
+    }
+    return count;
+}
+public:
+    int countPrimes(long long n) {
+        if(n==0 || n==1){
+            return 0;
+        }
+        else{
+        long long ans=sieveOfEratosthenes(n-1);
+        return ans;
+        }
+    }
+};
