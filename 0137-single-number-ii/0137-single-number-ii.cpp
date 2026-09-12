79ec7adc5c28ef767f72e033234cd48ee0aac112
raunakKapoor07 <raunak04kapoor@gmail.com>
2026-09-12 11:04:25 +0530

    Time: 3 ms (32.04%), Space: 13.4 MB (35.67%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> binary(32,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<32;j++){
+                binary[j]+=(nums[i]>>j) & 1;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<32;i++){
+            binary[i]=binary[i]%3;
+        }
+        for(int i=0;i<32;i++){
+            ans+=binary[i]*(1<<i);
+        }
+        return ans;
+    }
+};
